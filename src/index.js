7098327e36440e1985b13a19e87fe3e343cc042a
git config --global u ser.name Rohan-Suryawanshi <suryawanshi9673r@gmail.com>
2025-03-14 18:12:37 +0530

    Changes Done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDb from "./db/index.js";
-
+const PORT = process.env.PORT || 8000;
 dotenv.config({ path: "./env" });
-connectDb();
+ 
+connectDb()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+
+    app.on("error", (error) => {
+      console.log("ERROR : " + error);
+      process.exit(1);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB Connection FAIL !!" + error);
+  });
 
 // (async () => {
 //   try {
---
+++ src/models/user.model..js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //clourinary String
+      required: true,
+    },
+    coverImage: {
+      type: String, //clourinary String
+      required: true,
+    },
+    watchHistory: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
+      default: [],
+    },
+
+    password: { type: String, required: [true, "Password is Required"] },
+    refreshToken: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
